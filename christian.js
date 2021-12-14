caab168a6c2c4d56a2f9f9d19c20256c18d2c353
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2021-12-14 09:23:51 +0100

    Lösung von Christian

---
+++ christian.js
@@ -0,0 +1,9 @@
+let ergebnis = 1;
+for (let i = 20; i < 100; i++) {
+  ergebnis *= i;
+  //   console.log(ergebnis);
+}
+console.log(ergebnis);
+
+// Wenn wir bei 0 beginnen multiplizieren wir mit 0,
+// d.h. es kommt immer 0 raus.
